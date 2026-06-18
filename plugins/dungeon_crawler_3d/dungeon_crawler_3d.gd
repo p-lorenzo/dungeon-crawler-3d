d2c5b39f189b837a66051b943a0df2e0d703fa7b
p-lorenzo <lorenzo.pesce.1996@gmail.com>
2026-06-18 15:09:55 +0200

    fix: correct GDScript setter syntax and remove unsupported EditorPlugin API
    - Remove type annotations from setter parameters (e.g. set(value: int) -> set(value))
      as Godot 4.6.3 does not support typed setter parameters
    - Remove add_custom_resource_type / remove_custom_resource_type calls
      which are not available on EditorPlugin in Godot 4.6.3;
      class_name already registers resources globally
    
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

--- plugins/dungeon_crawler_3d/dungeon_crawler_3d.gd
+++ plugins/dungeon_crawler_3d/dungeon_crawler_3d.gd
@@ -8,12 +8,6 @@ const DungeonGenerator3DScript: Script = preload("res://plugins/dungeon_crawler_
 func _enter_tree() -> void:
 	add_custom_type("RoomConnector3D", "Node3D", RoomConnector3DScript, null)
 	add_custom_type("DungeonGenerator3D", "Node3D", DungeonGenerator3DScript, null)
-	add_custom_resource_type("DungeonConfig", "Resource", preload("res://plugins/dungeon_crawler_3d/resources/dungeon_config.gd"), null)
-	add_custom_resource_type("RoomData", "Resource", preload("res://plugins/dungeon_crawler_3d/resources/room_data.gd"), null)
-
-
 func _exit_tree() -> void:
 	remove_custom_type("RoomConnector3D")
 	remove_custom_type("DungeonGenerator3D")
-	remove_custom_resource_type("DungeonConfig")
-	remove_custom_resource_type("RoomData")
--- plugins/dungeon_crawler_3d/resources/dungeon_config.gd
+++ plugins/dungeon_crawler_3d/resources/dungeon_config.gd
@@ -4,28 +4,28 @@ extends Resource
 
 @export_category("Topology")
 @export var main_path_length: int = 5:
-	set(value: int):
+	set(value):
 		main_path_length = maxi(value, 1)
 @export var branch_count: int = 2:
-	set(value: int):
+	set(value):
 		branch_count = maxi(value, 0)
 @export var branch_depth_min: int = 1:
-	set(value: int):
+	set(value):
 		branch_depth_min = maxi(value, 0)
 @export var branch_depth_max: int = 3:
-	set(value: int):
+	set(value):
 		branch_depth_max = maxi(value, branch_depth_min)
 
 @export_category("Constraints")
 @export var room_count_min: int = 3:
-	set(value: int):
+	set(value):
 		room_count_min = maxi(value, 1)
 @export var room_count_max: int = 50:
-	set(value: int):
+	set(value):
 		room_count_max = maxi(value, room_count_min)
 @export var random_seed: int = 0
 @export var max_generation_attempts: int = 10:
-	set(value: int):
+	set(value):
 		max_generation_attempts = maxi(value, 1)
 
 @export_category("Room Pools")
